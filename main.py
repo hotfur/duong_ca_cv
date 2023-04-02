21e47c6be12a60617c66d4951cf47d267ae68416
Vuong Kha Sieu <sieu.vuong.200087@student.fulbright.edu.vn>
2023-04-02 15:30:16 +0700

    Fix type in colorspace conversion

--- main.py
+++ main.py
@@ -16,19 +16,19 @@ if __name__ == '__main__':
     path = '../../data/line_trace/congthanh_solution/'
     img = cv2.imread(cv2.samples.findFile(path + "0" + ".png"))
     # Gaussian filter to slightly reduce noise
-    img = cv2.GaussianBlur(img, ksize=(3,3), sigmaX=1, sigmaY=1)
+    img_blurred = cv2.GaussianBlur(img, ksize=(3,3), sigmaX=1, sigmaY=1)
     # Convert to Lab
-    src = cv2.cvtColor(img, cv2.COLOR_BGR2Lab)
+    img_blurred = cv2.cvtColor(img_blurred, cv2.COLOR_BGR2Lab)
     # Seperate the color components and lightning
-    lightning = img[:, :, 0]
+    lightning = img_blurred[:, :, 0]
     mean_lightning = np.mean(lightning)
-    color = img[:, :, 1:] - 127
+    color = img_blurred[:, :, 1:] - 127
     color_dist = np.sqrt(np.sum(color.__pow__(2), axis=2))
     # Apply adaptive thresholding for lightning matrix and global thresholding for color matrix
     # We must filter black pixel before feeding to otsu
     lightning = np.where(lightning>black_threshold, lightning, int(mean_lightning))
     _, lightning_thresh = cv2.threshold(lightning, 0, 255, cv2.THRESH_OTSU)
-    color_thresh = color_dist > color_distance_threshold
+    color_thresh = color_dist < color_distance_threshold
     mixture = np.logical_and(color_thresh, lightning_thresh.astype(bool))
     thresh_result = np.where(mixture, 255, 0).astype(np.int16)
     # Apply morphology transformation to filter noises
