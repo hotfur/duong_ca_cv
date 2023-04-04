49f7ddca630eb25a584d6e5aa06932384e5c9ac1
Vuong Kha Sieu <sieu.vuong.200087@student.fulbright.edu.vn>
2023-04-04 21:24:43 +0700

    Finalize line finding

--- 2.py
+++ 2.py
@@ -1,25 +1,26 @@
 # importing libraries
 import cv2
-import math
+import kornia as K
 import numpy as np
 import torch
-import kornia as K
+
 # Global constant
 color_distance_threshold = 8
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 # Line color
-line_color = (100,0,255)
-direction_line_color = (255,100,0)
-text_location = (50,100)
+line_color = (100, 0, 255)
+direction_line_color = (255, 100, 0)
+text_location = (50, 100)
 err = 0.0001
+
 
 def lane_making(img):
     arr = np.array(img)
-    arr[:,0], arr[:,-1] = arr[:,-1], arr[:,0]
+    arr[:, 0], arr[:, -1] = arr[:, -1], arr[:, 0]
     x_rgb = torch.tensor(arr, device=device).unsqueeze(0) / 255
-    x_rgb = torch.transpose(x_rgb, dim0=0, dim1=-1)[...,0]
+    x_rgb = torch.transpose(x_rgb, dim0=0, dim1=-1)[..., 0]
     x_rgb = x_rgb.unsqueeze(0)
-    # Convert to Lab
+    # Convert to Lab colorspace
     img_blurred = K.color.rgb_to_lab(x_rgb)
     # Seperate the color components and lightning
     lightning = img_blurred[0, 0, :, :]
@@ -31,46 +32,58 @@ def lane_making(img):
     color_thresh = color_dist < color_distance_threshold
     mixture = torch.logical_and(color_thresh, lightning)
     thresh_result = torch.where(mixture, 1.0, 0.0)
-    bw = torch.squeeze(thresh_result).cpu().numpy().astype(np.uint8)*255
-    contours = cv2.findContours(bw, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Return black and white image to CPU for contour finding
+    bw = torch.squeeze(thresh_result).cpu().numpy().astype(np.uint8) * 255
+    contours = cv2.findContours(bw, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_KCOS)
     contours = contours[0] if len(contours) == 2 else contours[1]
-    if len(contours) == 0: return
+    # Defensive programming
+    if len(contours) == 0:
+        return
+    # Filter contours by area and number of vertices
     contours_and_weights = []
     i = 0
     for cnt in contours:
         area = cv2.contourArea(cnt)
         if area > 0:
-            contours_and_weights.append((area/len(cnt), i, cnt))
-        i+=1
+            contours_and_weights.append((area, i, cnt))
+        i += 1
     contours_and_weights.sort(reverse=True)
+    # Filter contours by area powered by image momentum
     contours = []
-    for cnt in range(min(2, len(contours_and_weights))):
-        contours.append(contours_and_weights[cnt][2])
-    cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
-    for cnt in contours:
+    for cnt in range(min(6, len(contours_and_weights))):
+        contour = contours_and_weights[cnt][2]
+        moment = np.power(contours_and_weights[cnt][0], cv2.HuMoments(cv2.moments(contour))[0])
+        contours.append((moment, cnt, contour))
+    # Defensive programming
+    if len(contours) == 0:
+        return
+    # Take only the two best contours as line.
+    contours.sort(reverse=True)
+    contours = contours[:2]
+    # Write contours to image
+    for moment, i, cnt in contours:
         rows, cols = img.shape[:2]
         [vx, vy, x, y] = cv2.fitLine(cnt, cv2.DIST_L2, 0, 0.01, 0.01)
         lefty = int(np.clip((-x * vy / vx) + y, -10000, 10000))
         righty = int(np.clip(((cols - x) * vy / vx) + y, -10000, 10000))
-        cv2.line(img, (cols - 1, righty), (0, lefty), (0, 255, 0), 2)
-    # cv2.imshow('edges', img)
+        cv2.line(img, (cols - 1, righty), (0, lefty), line_color, 2)
+        cv2.drawContours(img, [cnt], 0, line_color, 3)
+    # cv2.drawContours(img, contours, -1, line_color, 3)
     return img
-# Create a VideoCapture object and read from input file
-cap = cv2.VideoCapture('../../data/line_trace/bacho/congthanh_solution.mp4')
-# Check if camera opened successfully
+
+
+cap = cv2.VideoCapture('../../data/line_trace/bacho/WIN_20230401_16_15_08_Pro.mp4')
+# For Jetson Nano
+# cap = cv2.VideoCapture('congthanh_solution.mp4')
 if not cap.isOpened():
     print("Error opening video file")
-# Read until video is completed
 output = cv2.VideoWriter("output.avi", cv2.VideoWriter_fourcc(*'MJPG'), 10, (int(cap.get(3)), int(cap.get(4))))
 while cap.isOpened():
-    # Capture frame-by-frame
     ret, frame = cap.read()
     if not ret:
         break
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        break
-    processed=lane_making(frame)
+    processed = lane_making(frame)
     output.write(processed)
-# When everything done, release the video capture object & Closes all the frames
+# When everything done, release the video capture object
 cap.release()
-cv2.destroyAllWindows()
+output.release()
--- main.py
+++ main.py
@@ -1,128 +1,89 @@
-"""Program to detect line
-@author Vuong Kha Sieu
-@author Nguyen Hoang An
-@author Le Thai Bach
-"""
-
+# importing libraries
 import cv2
-import numpy as np
-import queue
-import torch
 import kornia as K
+import numpy as np
+import torch
 
 # Global constant
 color_distance_threshold = 8
-black_threshold = 50
-morphology_iterations = 3
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# Line color
+line_color = (100, 0, 255)
+direction_line_color = (255, 100, 0)
+text_location = (50, 100)
+err = 0.0001
 
-if __name__ == '__main__':
-    path = '../../data/line_trace/congthanh_solution/'
-    img = cv2.imread(cv2.samples.findFile(path + "0" + ".png"))
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    x_bgr: torch.tensor = K.image_to_tensor(img)
-    x_bgr = x_bgr.unsqueeze(0)
-    x_bgr = x_bgr.to(device)
-    x_rgb: torch.tensor = K.color.bgr_to_rgb(x_bgr) / 255
-    # Gaussian filter to slightly reduce noise
-    gauss = K.filters.GaussianBlur2d(kernel_size=(3,3), sigma=(1.0,1.0))
-    img_blurred = gauss(x_rgb)
-    # Convert to Lab
-    img_blurred = K.color.rgb_to_lab(img_blurred)
+
+def lane_making(img):
+    arr = np.array(img)
+    arr[:, 0], arr[:, -1] = arr[:, -1], arr[:, 0]
+    x_rgb = torch.tensor(arr, device=device).unsqueeze(0) / 255
+    x_rgb = torch.transpose(x_rgb, dim0=0, dim1=-1)[..., 0]
+    x_rgb = x_rgb.unsqueeze(0)
+    # Convert to Lab colorspace
+    img_blurred = K.color.rgb_to_lab(x_rgb)
     # Seperate the color components and lightning
-    img_blurred = img_blurred.squeeze(0)
-    lightning = img_blurred[0, :, :]
-    mean_lightning = torch.mean(lightning)
-    color = img_blurred[1:, :, :]
+    lightning = img_blurred[0, 0, :, :]
+    std_mean_lightning = torch.std_mean(lightning)
+    color = img_blurred[0, 1:, :, :]
     color_dist = torch.sqrt(torch.sum(torch.pow(color, exponent=2), dim=0))
     # Apply adaptive thresholding for lightning matrix and global thresholding for color matrix
-    # We must filter black pixel before feeding to otsu
-    lightning = torch.where(lightning > black_threshold, lightning, mean_lightning)
+    lightning = lightning > (std_mean_lightning[0] + std_mean_lightning[1])
     color_thresh = color_dist < color_distance_threshold
-    lightning = lightning.to("cpu").numpy().astype(np.uint16)
-    _, lightning_thresh = cv2.threshold(lightning, 0, 255, cv2.THRESH_OTSU)
-    lightning_thresh = torch.tensor(lightning_thresh.astype(bool), device=device)
-    mixture = torch.logical_and(color_thresh, lightning_thresh)
-    thresh_result = torch.where(mixture, 1, 0)
-    # Apply morphology transformation to filter noises
-    kernel = torch.tensor([[0, 1, 0],[1, 1, 1],[0, 1, 0]], dtype=torch.float32, device=device)
-    morph = K.morphology.dilation(thresh_result[None,None,...], kernel=kernel)
-    morph = K.morphology.dilation(morph, kernel=kernel)
-    # Apply median filter to remove excessive noises
-    bw = K.filters.median_blur(morph, kernel_size=5)
-    # Applying the Canny Edge filter
-    _, edges = K.filters.canny(bw, 0.01, 0.99)
-    edges = edges.squeeze()*255
-    edges = edges.to("cpu").numpy().astype(np.uint8)
-    all_highlighted_area = torch.sum(bw)
-    bw = bw.bool()
-    lines = cv2.HoughLines(edges, 1, np.pi / 90, 50, None, 0, 0)
-    searched_lines = set()
-    _, _, h, w = bw.shape
-    master_q = queue.PriorityQueue()
-    for line1_indx in range(10):
-        aux_q = queue.PriorityQueue()
-        line1 = lines[line1_indx]
-        searched_lines.add(line1_indx)
-        for line2_indx in range(15):
-            line2 = lines[line2_indx]
-            if line2_indx not in searched_lines:
-                line_area = np.zeros((h, w), dtype=np.int16)
-                pt11 = (int(line1[0][0] / np.cos(line1[0][1])), 0)
-                pt12 = (int((line1[0][0] - (h - 1) * np.sin(line1[0][1])) / np.cos(line1[0][1])), h - 1)
-                pt21 = (int(line2[0][0] / np.cos(line2[0][1])), 0)
-                pt22 = (int((line2[0][0] - (h - 1) * np.sin(line2[0][1])) / np.cos(line2[0][1])), h - 1)
-                pts = np.array([pt11, pt12, pt21, pt22])
-                cv2.fillPoly(line_area, [pts], color=1)
-                line_area = torch.tensor(line_area, device=device).bool()
-                true_area = torch.sum(torch.logical_and(line_area, bw))
-                good = true_area / torch.sum(line_area)
-                significant = true_area / all_highlighted_area
-                if good > 0.5 and significant > 0.05:
-                    try: aux_q.put((1 - significant, line1, line2))
-                    except: continue
-        if not aux_q.empty():
-            try:
-                best_pair = aux_q.get()
-                master_q.put(best_pair)
-            except:
-                continue
-    searched_lines = []
-    #for i in range(6):
-    while not master_q.empty():
-        priority, line1, line2 = master_q.get()
-        searched_lines.append(line1)
-        searched_lines.append(line2)
-    for r_theta in searched_lines:
-        arr = np.array(r_theta[0], dtype=np.float64)
-        r, theta = arr
-        # Stores the value of cos(theta) in a
-        a = np.cos(theta)
-
-        # Stores the value of sin(theta) in b
-        b = np.sin(theta)
-
-        # x0 stores the value rcos(theta)
-        x0 = a * r
-
-        # y0 stores the value rsin(theta)
-        y0 = b * r
-
-        # x1 stores the rounded off value of (rcos(theta)-1000sin(theta))
-        x1 = int(x0 + 1000 * (-b))
-
-        # y1 stores the rounded off value of (rsin(theta)+1000cos(theta))
-        y1 = int(y0 + 1000 * (a))
-
-        # x2 stores the rounded off value of (rcos(theta)+1000sin(theta))
-        x2 = int(x0 - 1000 * (-b))
-
-        # y2 stores the rounded off value of (rsin(theta)-1000cos(theta))
-        y2 = int(y0 - 1000 * (a))
-
-        # cv2.line draws a line in img from the point(x1,y1) to (x2,y2).
-        # (0,0,255) denotes the colour of the line to be
-        # drawn. In this case, it is red.
-        cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
-    cv2.imwrite("test.jpg", img)
+    mixture = torch.logical_and(color_thresh, lightning)
+    thresh_result = torch.where(mixture, 1.0, 0.0)
+    # Return black and white image to CPU for contour finding
+    bw = torch.squeeze(thresh_result).cpu().numpy().astype(np.uint8) * 255
+    contours = cv2.findContours(bw, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_KCOS)
+    contours = contours[0] if len(contours) == 2 else contours[1]
+    # Defensive programming
+    if len(contours) == 0:
+        return
+    # Filter contours by area and number of vertices
+    contours_and_weights = []
+    i = 0
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 0:
+            contours_and_weights.append((area, i, cnt))
+        i += 1
+    contours_and_weights.sort(reverse=True)
+    # Filter contours by area powered by image momentum
+    contours = []
+    for cnt in range(min(6, len(contours_and_weights))):
+        contour = contours_and_weights[cnt][2]
+        moment = np.power(contours_and_weights[cnt][0], cv2.HuMoments(cv2.moments(contour))[0])
+        contours.append((moment, cnt, contour))
+    # Defensive programming
+    if len(contours) == 0:
+        return
+    # Take only the two best contours as line.
+    contours.sort(reverse=True)
+    contours = contours[:2]
+    # Write contours to image
+    for moment, i, cnt in contours:
+        rows, cols = img.shape[:2]
+        [vx, vy, x, y] = cv2.fitLine(cnt, cv2.DIST_L2, 0, 0.01, 0.01)
+        lefty = int(np.clip((-x * vy / vx) + y, -10000, 10000))
+        righty = int(np.clip(((cols - x) * vy / vx) + y, -10000, 10000))
+        cv2.line(img, (cols - 1, righty), (0, lefty), line_color, 2)
+        cv2.drawContours(img, [cnt], 0, line_color, 3)
+    # cv2.drawContours(img, contours, -1, line_color, 3)
+    return img
 
 
+cap = cv2.VideoCapture('../../data/line_trace/bacho/WIN_20230401_16_15_08_Pro.mp4')
+# For Jetson Nano
+# cap = cv2.VideoCapture('congthanh_solution.mp4')
+if not cap.isOpened():
+    print("Error opening video file")
+output = cv2.VideoWriter("output.avi", cv2.VideoWriter_fourcc(*'MJPG'), 10, (int(cap.get(3)), int(cap.get(4))))
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+    processed = lane_making(frame)
+    output.write(processed)
+# When everything done, release the video capture object
+cap.release()
+output.release()
