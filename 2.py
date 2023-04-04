0226cc30244f9ed58dd052920f3fea597e76404e
Vuong Kha Sieu <sieu.vuong.200087@student.fulbright.edu.vn>
2023-04-04 14:37:57 +0700

    Contours finding

--- 2.py
+++ 2.py
@@ -62,57 +62,23 @@ def lane_making(img):
     color_thresh = color_dist < color_distance_threshold
     mixture = torch.logical_and(color_thresh, lightning)
     thresh_result = torch.where(mixture, 1.0, 0.0)
-    # Applying the Canny Edge filter
-    _, edges = K.filters.canny(thresh_result[None,None,...], 0.01, 0.99)
-    edges = torch.squeeze(edges).cpu().numpy().astype(np.uint8)*255
-    bw = thresh_result.bool()
-    all_highlighted_area = torch.sum(bw)
-    lines = cv2.HoughLines(edges, 1, np.pi / 60, 50, None, 0, 0)
-    if lines is None: return
-    master_q = queue.PriorityQueue()
-    with ThreadPoolExecutor() as e:
-        for line1_indx in range(min(num_lines, len(lines))):
-            e.submit(find_best_lines,master_q,lines, line1_indx, bw.shape, bw, all_highlighted_area)
-    searched_lines = []
-    while not master_q.empty():
-        try:
-            priority, line1, line2 = master_q.get()
-        except:
-            break
-        searched_lines.append(line1)
-        searched_lines.append(line2)
-    for r_theta in searched_lines:
-        arr = np.array(r_theta[0], dtype=np.float64)
-        r, theta = arr
-        # Stores the value of cos(theta) in a
-        a = np.cos(theta)
-
-        # Stores the value of sin(theta) in b
-        b = np.sin(theta)
-
-        # x0 stores the value rcos(theta)
-        x0 = a * r
-
-        # y0 stores the value rsin(theta)
-        y0 = b * r
-
-        # x1 stores the rounded off value of (rcos(theta)-1000sin(theta))
-        x1 = int(x0 + 1000 * (-b))
-
-        # y1 stores the rounded off value of (rsin(theta)+1000cos(theta))
-        y1 = int(y0 + 1000 * (a))
-
-        # x2 stores the rounded off value of (rcos(theta)+1000sin(theta))
-        x2 = int(x0 - 1000 * (-b))
-
-        # y2 stores the rounded off value of (rsin(theta)-1000cos(theta))
-        y2 = int(y0 - 1000 * (a))
-
-        # cv2.line draws a line in img from the point(x1,y1) to (x2,y2).
-        # (0,0,255) denotes the colour of the line to be
-        # drawn. In this case, it is red.
-        cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
-    #cv2.imshow('edges', img)
+    # # Applying the Canny Edge filter
+    # _, edges = K.filters.canny(thresh_result[None,None,...], 0.01, 0.99)
+    # edges = torch.squeeze(edges).cpu().numpy().astype(np.uint8)*255
+    bw = torch.squeeze(thresh_result).cpu().numpy().astype(np.uint8)*255
+    contours = cv2.findContours(bw, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
+    contours_and_weights = []
+    i = 0
+    for cnt in contours:
+        contours_and_weights.append((cv2.contourArea(cnt), i, cnt))
+        i+=1
+    contours_and_weights.sort(reverse=True)
+    contours = []
+    for cnt in range(4):
+        contours.append(contours_and_weights[cnt][2])
+    cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
+    cv2.imshow('edges', img)
     return img
 # Create a VideoCapture object and read from input file
 cap = cv2.VideoCapture('../../data/line_trace/bacho/WIN_20230401_16_16_01_Pro.mp4')
