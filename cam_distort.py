b7e1b6508e96e3055c99c56551068788bdf8a7a8
Lê Thái Bách <95233211+FUVHasagi@users.noreply.github.com>
2023-04-05 17:42:03 +0700

    Add files via upload

--- cam_distort.py
+++ cam_distort.py
@@ -4,29 +4,37 @@ import cv2 as cv
 import glob
 import yaml
 import os
+h, w = (960, 1280) # image size in pixels
 
-def undistort(img, yaml_file):
+def undistort_point(x, y, yaml_file):
+    x = np.array(x)
+    y = np.array(y)
+    with open(yaml_file, "r") as f:
+        data = yaml.load(f, Loader = yaml.loader.SafeLoader)
+        print(data)
+        mtx=np.array(data['camera_matrix'])
+        
+        dist=np.array(data['dist_coeff'])
+    r = np.sqrt(x**2+y**2)
+    
+def unpack_yaml(yaml_file):
     with open(yaml_file, "r") as f:
         data = yaml.load(f, Loader = yaml.loader.SafeLoader)
         print(data)
         mtx=np.array(data['camera_matrix'])
         dist=np.array(data['dist_coeff'])
-    h, w = img.shape[0], img.shape[1]
-    # Refining the camera matrix using parameters obtained by calibration
     newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
-    
-    # Method 1 to undistort the image
-    dst = cv.undistort(img, mtx, dist, None, newcameramtx)
-    
-    # # Method 2 to undistort the image
-    # mapx, mapy = cv.initUndistortRectifyMap(mtx,dist,None,newcameramtx,(w,h),5)
-    
-    # dst = cv.remap(img,mapx,mapy,cv.INTER_LINEAR)
+    # Method 2 to undistort the image
+    mapx, mapy = cv.initUndistortRectifyMap(mtx,dist,None,newcameramtx,(w,h),5)
+    return mapx, mapy
+
+def undistort(img, mapx, mapy):       
+    dst = cv.remap(img,mapx,mapy,cv.INTER_LINEAR)
     return dst
     
-def undistort_file(input_file, yaml_file, output_path = './'):
+def undistort_file(input_file, mapx, mapy, output_path = './'):
     img = cv.imread(input_file)
-    dst = undistort(img, yaml_file)
+    dst = undistort(img, mapx, mapy)
     f = input_file.split('/')[-1]
     name = f.split('.')
     img_RGB = cv.imread(input_file)
