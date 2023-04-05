29b0c089c1ffdba927a31e960d6480d872e65952
annguyen200011 <an.nguyen.200011@student.fulbright.edu.vn>
2023-04-05 19:33:27 +0700

    Add jupyter for testing

--- jetson.py
+++ jetson.py
@@ -14,12 +14,12 @@ pixel_to_mm = 3.285521454
 robot_height = 215
 robot_wide = 300
 # Line color
-line_color = (255, 100, 0)
+line_color = (255, 0, 0)
 direction_line_color = (100, 0, 255)
 text_location1 = (50, 130)
 text_location2 = (50, 160)
 text_location3 = (50, 190)
-text_location4 = (50, 210)
+text_location4 = (50, 230)
 font = cv2.FONT_HERSHEY_SIMPLEX
 err = 0.0001
 # Camera matrix
@@ -49,6 +49,16 @@ def find_ax_by_c(x1, y1, x2, y2):
 
 def calc_distance(pixel):
     return np.sqrt(robot_wide**2 + robot_height**2 + (pixel*pixel_to_mm)**2)
+
+def find_mid_dis(a_mid, b_mid, y_lower):
+    y = 640
+    x = (y-b_mid)/(a_mid + err)
+    if x < 720 and x > 0:
+        return y_lower*(robot_wide/np.abs(720-x) + 1)
+    elif x < 0:
+        return y_lower*(robot_wide/np.abs(720+x) + 1)
+    else:
+        return y_lower*(robot_wide/np.abs(x-720) - 1)
 
 def mid_line(left_line, right_line, err=0.0001):
     ptl1, ptl2 = left_line[0], left_line[1]
@@ -169,9 +179,9 @@ angle, speed_feedback = 0, 0
 while cap.isOpened():
     # Capture frame-by-frame
     ret, frame = cap.read()
-    if num_frame == 1000:
-        break
     if not ret:
+        break
+    if num_frame == 3000:
         break
     # Too much computing power for undistort, so use only on PC
     # frame = undistort(frame)
@@ -184,6 +194,9 @@ while cap.isOpened():
             _mid_line = mid_line(_left_line, _right_line)
             # distance midline & mid of image
             a_mid, b_mid = _mid_line[2]
+            y_lower = 1280/2 - _mid_line[0][0]
+            dis_mid = find_mid_dis(a_mid, b_mid, y_lower)
+
             camera_axis_to_mid = np.clip(w // 2 - _mid_line[0][0], -w // 2, w // 2)
             angle = (angle + learning_rate * np.clip(a_mid, -np.pi/4, np.pi/4)) / (1 + learning_rate)
             speed_feedback_raw = np.clip(0.5 - _mid_line[1][0]/w, -0.5, 0.5)
@@ -195,7 +208,8 @@ while cap.isOpened():
     cv2.putText(frame, 'speed_feedback: ' + str(round(speed_feedback, 3)), text_location1, font, 1, line_color, 1, cv2.LINE_AA)
     cv2.putText(frame, 'angle: ' + str(round(np.rad2deg(angle), 3)), text_location2, font, 1, line_color, 1, cv2.LINE_AA)
     cv2.putText(frame, 'camera_axis_to_mid: ' + str(camera_axis_to_mid), text_location3, font, 1, line_color, 1, cv2.LINE_AA)
-    cv2.putText(frame, 'camera_axis_to_mid: ' + str(round(calc_distance(camera_axis_to_mid), 3)), text_location4, font, 1, line_color, 1, cv2.LINE_AA)
+    #cv2.putText(frame, 'camera_axis_to_mid: ' + str(round(calc_distance(camera_axis_to_mid), 3)), text_location4, font, 1, line_color, 1, cv2.LINE_AA)
+    cv2.putText(frame, 'dis_mid: ' + str(round(dis_mid, 3)), text_location4, font, 1, line_color, 1, cv2.LINE_AA)
     cv2.circle(frame, (w//2, h), 5, direction_line_color, -1)
     output.write(frame)
 
